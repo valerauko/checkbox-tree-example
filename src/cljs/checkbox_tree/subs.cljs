c740074c2556d49b699ef69c6deae273b68cee51
Vale <valerauko@gmail.com>
2020-06-11 23:43:18 +0900

    Add subs
    These are re-frame's take on getters

--- src/cljs/checkbox_tree/subs.cljs
+++ src/cljs/checkbox_tree/subs.cljs
@@ -6,3 +6,18 @@
  ::name
  (fn [db]
    (:name db)))
+
+(re-frame/reg-sub
+ ::tree-struct
+ (fn [db]
+   (:tree-struct db)))
+
+(re-frame/reg-sub
+ ::items-checked
+ (fn [db _]
+   (:checked db)))
+
+(re-frame/reg-sub
+ ::items-expanded
+ (fn [db _]
+   (:expanded db)))
