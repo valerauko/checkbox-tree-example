055881f6e090afa34f03ecb9a13b7bcebdaae596
Vale <valerauko@gmail.com>
2020-06-11 22:46:29 +0900

    Add initial DB state
    This could just as well be loaded from some external source (like local-storage)

--- src/cljs/checkbox_tree/db.cljs
+++ src/cljs/checkbox_tree/db.cljs
@@ -1,4 +1,19 @@
 (ns checkbox-tree.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:name "CheckboxTree example"
+   :tree-struct
+   [{:value "mars"
+     :label "Mars"
+     :children
+     [{:value "phobos"
+       :label "Phobos"}
+      {:value "deimos"
+       :label "Deimos"}]}
+    {:value "earth"
+     :label "Earth"
+     :children
+     [{:value "moon"
+       :label "Moon"}]}]
+   :expanded []
+   :checked []})
