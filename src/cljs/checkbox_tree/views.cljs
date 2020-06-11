d96381f77e26ce09a7109860eeded2856ae4201b
Vale <valerauko@gmail.com>
2020-06-11 23:55:43 +0900

    Set up CheckboxTree component

--- src/cljs/checkbox_tree/views.cljs
+++ src/cljs/checkbox_tree/views.cljs
@@ -7,7 +7,16 @@
    ))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
+  (let [name (re-frame/subscribe [::subs/name])
+        tree-struct (re-frame/subscribe [::subs/tree-struct])
+        checked (re-frame/subscribe [::subs/items-checked])
+        expanded (re-frame/subscribe [::subs/items-expanded])]
     [:div
-     [:h1 "Hello from " @name]
-     ]))
+     [:h1 "This is " @name]
+     [:> CheckboxTree
+      {:nodes @tree-struct
+       :checked @checked
+       :expanded @expanded
+       :show-expand-all true
+       :on-check #(re-frame/dispatch [::events/check-item %])
+       :on-expand #(re-frame/dispatch [::events/expand-item %])}]]))
